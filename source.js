f14d9400fed49ac6732297384e81a99cae5b9709
Samuca <35107922+Samuuuca@users.noreply.github.com>
2022-05-31 21:41:46 -0300

    Update source.js

--- source.js
+++ source.js
@@ -1,26 +1,42 @@
+
+
 function run() {
 
     const result = document.getElementById('Resultado')
-    let { desmotivacional, demo2, desmotivacional_not } = arrays()
-    let number = Math.floor(Math.random() * demo2.length)
-    console.log(number);
-
+        result.style.display = 'block'
+    let { desmotivacional, demo2, fixo, sufixo } = arrays()
     
+    let number = Math.floor(Math.random() * demo2.length)
+        console.log(number);
+
+    const number1 = Math.floor(Math.random() * fixo.length)
+        console.info(number1)
+
     if (number == 0) {
 
     const number0 = desmotivacional[Math.floor(Math.random() * desmotivacional.length)]
-    let frase = 'Você ' + demo2[0] +'um '+ number0;
+        let frase = 'Você ' + demo2[0] +'um '+ number0;
     result.innerHTML = frase;
        
-   } if (number == 1) {
-    const number1 = desmotivacional_not[Math.floor(Math.random() * desmotivacional_not.length)]
-    let frase = 'Você '+ demo2[1] + number1;
+   } if (number == 1 && number1 == 0){
+
+    const sufixo1 = sufixo[Math.floor(Math.random() * sufixo.length)];
+        let frase = 'Você '+ demo2[1] + fixo[number1];
+    result.innerHTML = frase;
+
+
+   } if (number == 1 && number1 == 1) { //Se cair o serve
+    
+    const sufixo1 = sufixo[Math.floor(Math.random() * sufixo.length)];
+        let frase = 'Você '+ demo2[1] + fixo[number1] + ' pra ' + sufixo1;
     result.innerHTML = frase;
        
    } else {
-       return
+       console.error
    } 
 
+
+   console.log('---------------------------------------')
     
 }
 
@@ -35,9 +51,14 @@ function arrays() {
         'é ',
         'não '
     ]
-    let desmotivacional_not = [
+    let fixo = [
         'presta',
         'serve',
     ]
-    return { desmotivacional, demo2, desmotivacional_not }
+    let sufixo =[
+        'nada',
+        'lavar uma louça',
+
+    ]
+    return { desmotivacional, demo2, fixo, sufixo }
 }
